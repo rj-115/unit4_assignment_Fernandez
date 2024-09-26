d0dc99bbe07a40de793c7d6c9749ed013ccff299
rj-115 <rhestyjohn.fernandez@wvsu.edu.ph>
2024-09-26 10:42:38 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:unit4_assignment_fernandez/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyProfileApp());
+    await tester.pumpWidget(MyProfile());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
